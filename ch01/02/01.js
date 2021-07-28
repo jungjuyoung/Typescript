d80820da01d4fd41b0d3126845814571bac470f6
jungjuyoung <nwwn1004@naver.com>
2021-07-29 00:43:50 +0900

    feat: item2 complete

---
+++ ch01/02/01.js
@@ -0,0 +1,8 @@
+"use strict";
+// tsConfig: {"noImplicitAny":false,"strictNullChecks":false}
+exports.__esModule = true;
+function add(a, b) {
+    return a + b;
+}
+add(10, null);
+exports["default"] = {};
--- ch01/02/07.ts
+++ ch01/02/07.ts
@@ -1,5 +1,8 @@
 // tsConfig: {"noImplicitAny":true,"strictNullChecks":true}
 
 const x: number | null = null
+// const x: number | string = ''
+// const x: number | string = 1
+// typescript에서 type의 || 를 | 하나로 사용하면 된다.
 
 export default {}
