c6cfa2f1eeff387577766172e929103fa21364ba
jungjuyoung <nwwn1004@naver.com>
2021-08-03 23:51:27 +0900

    feat: item07 review

--- ch02/07/19.ts
+++ ch02/07/19.ts
@@ -1,7 +1,7 @@
 type T = Exclude<string | Date, string | number> // Type is Date
+// 좌측을 string | Date 기준으로 우측의 string | number을 배제한다.
+// 그러니까 string | number 유니언중에 해당하는것 들을 제외한다.
+// 그러니까 Date만 남음.
 type NonZeroNums = Exclude<number, 0> // Type is still just number
-type NonZeroNums2 = Exclude<number | 0, string | 0> // Type is still just number
-
-const a:NonZeroNums2 = 4
 
 export default {}
