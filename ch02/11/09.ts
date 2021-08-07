d4bdde20613fc4d854d0cf360973cc9d6a9f16e8
jungjuyoung <nwwn1004@naver.com>
2021-08-08 01:36:15 +0900

    feat: added futher explanation about  index signiture

--- ch02/11/09.ts
+++ ch02/11/09.ts
@@ -1,6 +1,7 @@
 interface Options {
   darkMode?: boolean // darkMode 선택적으로 있어도 되고 없어도 되고, 
-  [otherOptions: string]: unknown // 추가되는 다른 애들은 아무렇게나 오게
+  [otherOptions: string]: unknown // 인덱스 시그니처
+  // [othersOptions:string]으로 추가되는 다른 애들은 아무렇게나 오게
 }
 const o: Options = { darkmode: true } // OK
 
