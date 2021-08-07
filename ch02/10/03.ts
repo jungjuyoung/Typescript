f0ff678f32d9c8abb582ddd01ccb8e4e17523341
jungjuyoung <nwwn1004@naver.com>
2021-08-07 16:00:59 +0900

    feat: item10 review add more

--- ch02/10/03.ts
+++ ch02/10/03.ts
@@ -5,7 +5,8 @@ function isGreeting(phrase: String) {
   // of type 'string'.
   // 'string' is a primitive, but 'String' is a wrapper object;
   // prefer using 'string' when possible
-  // 아하, String은 string을 감싼 wrapper object라서 통과됐던거임, 가능하면 string 사용하는것을 선호.
+  // 아하, String은 string을 감싼 wrapper object라서 통과됐던거임, 가능하면 string 사용하는것을 선호. 고로 'string' => String은 통과, String => string은 불통.
+  
 }
 
 export default {}
